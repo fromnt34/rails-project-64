a788a6ba36251bd462c94150ac673feef4a62018
Dmitry Kurtsev <dmitrykurtsev34@gmail.com>
2023-01-09 15:50:13 +0300

    Создал регистрацию и авторизацию, а также навигацию

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_112443) do
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
