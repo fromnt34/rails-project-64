6ebdafd30532e44231aeddbf4690a77739dec04f
Dmitry Kurtsev <dmitrykurtsev34@gmail.com>
2023-01-27 18:47:21 +0300

    Рефакторинг

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
     @user_like = @post.find_like(current_user) if current_user
 
-    @comments = @post.comments.arrange
+    @comments = @post.comments.includes(:user).arrange
     @new_comment = PostComment.new
   end
 
