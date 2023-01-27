5af7d13824b2d14e0cc89072036753bc6f05918f
Dmitry Kurtsev <dmitrykurtsev34@gmail.com>
2023-01-27 16:34:13 +0300

    Редирект на страницу входа, если пользователь не залогинился

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  private
-
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to new_user_session_path, alert: t('devise.failure.unauthenticated')
-    end
-  end
 end
